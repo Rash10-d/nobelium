d7ce82bbc4206bf7aabeff59f9f84c4ec500fb0f
Rash10-d <75206698+Rash10-d@users.noreply.github.com>
2022-09-24 20:00:02 +0800

    Update blog.config.js
    更改了描述，更改了链接，关闭了关于

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'Enzoding-blog',
+  title: 'Enzoding的博客',
   author: 'Enzoding',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: '',
+  email: 'dxl20230805@163.com',
+  link: 'https://peaceding.top',
+  description: '一个写字的地方',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
@@ -13,7 +13,7 @@ const BLOG = {
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 10,
   sortByDate: false,
-  showAbout: true,
+  showAbout: false,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
